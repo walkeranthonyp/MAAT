3fe497609282b1efe44d1139afb9859d1d54427c
walkeranthonyp <walkeranthonyp@gmail.com>
2016-10-11 21:06:15 -0400

    test transition ci and stomatal limitation functions. Add them to the leaf_object test functions. Correct units for ri

--- src/unit_testing.R
+++ src/unit_testing.R
@@ -28,7 +28,10 @@ leaf_object$pars
 
 leaf_object$.test_leaf(verbose=F,leaf.par=1000,leaf.ca_conc=1100)
 leaf_object$.test_aci()
+leaf_object$.test_aci(diag=T)
+leaf_object$.test_aci(diag=T,leaf.par=c(450,900) )
 leaf_object$.test_aci_light()
+leaf_object$.test_aci_light(diag=T)
 
 
 
