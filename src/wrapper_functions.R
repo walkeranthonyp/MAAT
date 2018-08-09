2a842617de10455d7a9ca0ca436dad7e07acb57b
Johnson <johnsonal@ornl.gov>
2018-08-09 14:53:55 -0400

    troubleshooting when se's are 0

--- src/wrapper_functions.R
+++ src/wrapper_functions.R
@@ -36,11 +36,18 @@ f_proposal_lklihood_ssquared <- function(.) {
 f_proposal_lklihood_ssquared_se <- function(.) {
   # read in measurement error
   meas_sigma <- .$dataf$obsse
-  # number of measured data points
-  measurment_num <- length(.$dataf$obs)
+  # remove zeros from measurement error  
+  subset1  <- sapply(meas_sigma, function(row) all(row > 0))
+# change this!
+  meas_sigma2 <- meas_sigma[subset1]
+  # number of measured data points (that are not zero)
+  # measurment_num <- length(.$dataf$obs)
+  measurment_num <- length(meas_sigma2)
   # calculate error residual
   # each chain is on rows of dataf$out, take transpose
   error_residual_matrix <- t(.$dataf$out) - .$dataf$obs
+  # subset error_residual_matrix
+  error_residual_matrix2 <- error_residual_matrix[subset1, ]
   # derive the log density
   if (length(meas_sigma) == 1) {
     # homoscedastic error
@@ -50,12 +57,13 @@ f_proposal_lklihood_ssquared_se <- function(.) {
   }
   else {
     # heteroscedastic error
-    err_div_sig <- error_residual_matrix / as.vector(meas_sigma)
+    err_div_sig <- error_residual_matrix2 / as.vector(meas_sigma2)
     sum_err_div_sig <- apply(err_div_sig, 2, function(v) sum(v^2))
     # not sure if it is correct to take absolute value here???
-    log_density <- -(measurment_num/2)*log(2*pi) - sum(log(abs(meas_sigma))) - (1/2)*sum_err_div_sig
+    log_density <- -(measurment_num/2)*log(2*pi) - sum(log(abs(meas_sigma2))) - (1/2)*sum_err_div_sig
   }  
   # return log-likelihood vector corresponding to each chain/row in .$dataf$pars matrix
+  print(log_density)
   log_density
 } 
 
