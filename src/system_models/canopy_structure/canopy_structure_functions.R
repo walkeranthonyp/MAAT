895520484dda017c27fc51a13bb6107beeeb2b17
AnthonyWalker <walkeranthonyp@gmail.com>
2019-07-03 17:10:30 -0400

    refactor: canopy structure object to new template and OOP structure, unit tests working

--- src/system_models/canopy_structure/canopy_structure_functions.R
+++ src/system_models/canopy_structure/canopy_structure_functions.R
@@ -11,43 +11,43 @@
 # LAI
 ################################
 
-f_lai_constant <- function(.) .$pars$lai
+f_lai_constant <- function(.) .super$pars$lai
 
-f_lai_env      <- function(.) .$env$lai
+f_lai_env      <- function(.) .super$env$lai
 
 f_lai_leafdem_env <- function(.) {
-   .$state$lai_young  <- .$env$lai_young 
-   .$state$lai_mature <- .$env$lai_mature
-   .$state$lai_old    <- .$env$lai_old
+   .super$state$lai_young  <- .super$env$lai_young 
+   .super$state$lai_mature <- .super$env$lai_mature
+   .super$state$lai_old    <- .super$env$lai_old
 
-   .$state$lai_young + .$state$lai_mature + .$state$lai_old
+   .super$state$lai_young + .super$state$lai_mature + .super$state$lai_old
 }
 
 # calculate sphagnum 'lai' as a logistic function of water table depth
 f_lai_sphagnum <- function(.) {
   b <- 0.1
     
-  .$pars$lai_max*b / (b + exp(.$pars$lai_curve*(.$env$water_td-.$env$sphag_h)/10) )
+  .super$pars$lai_max*b / (b + exp(.super$pars$lai_curve*(.super$env$water_td-.super$env$sphag_h)/10) )
 }
 
 
 # Other stuff 
 ################################
 
-f_leafdem_vcmax0_constant <- function(.) unlist(.$pars$vcmax0)
+f_leafdem_vcmax0_constant <- function(.) unlist(.super$pars$vcmax0)
 
 
 f_leafdem_upper_wu2017 <- function(.) {
-  prop_young  <- .$state$lai_young  * .$pars$lai_ftop / .$pars$lai_upper
-  prop_mature <- .$state$lai_mature * .$pars$lai_ftop / .$pars$lai_upper
+  prop_young  <- .super$state$lai_young  * .super$pars$lai_ftop / .super$pars$lai_upper
+  prop_mature <- .super$state$lai_mature * .super$pars$lai_ftop / .super$pars$lai_upper
 
   c(prop_young, prop_mature, 1 - prop_young - prop_mature )
 }
 
 
 f_leafdem_lower_wu2017 <- function(.) {
-  prop_young  <- .$state$lai_young  * (1-.$pars$lai_ftop) / (.$state$lai-.$pars$lai_upper)
-  prop_mature <- .$state$lai_mature * (1-.$pars$lai_ftop) / (.$state$lai-.$pars$lai_upper)
+  prop_young  <- .super$state$lai_young  * (1-.super$pars$lai_ftop) / (.super$state$lai-.super$pars$lai_upper)
+  prop_mature <- .super$state$lai_mature * (1-.super$pars$lai_ftop) / (.super$state$lai-.super$pars$lai_upper)
 
   c(prop_young, prop_mature, 1 - prop_young - prop_mature )
 }
@@ -56,11 +56,11 @@ f_leafdem_lower_wu2017 <- function(.) {
 # Canopy/Leaf water status  
 ################################
 
-f_water_status_none <- function(.) .$leaf$state$fwdw_ratio <- NA
+f_water_status_none <- function(.) .super$leaf$state$fwdw_ratio <- NA
 
 # set sphagnum water status
 f_water_status_sphagnum <- function(.) {
-  .$leaf$state$fwdw_ratio <- get(.$fnames$fwdw)(.) 
+  .super$leaf$state$fwdw_ratio <- .$fns$fwdw()
 }
 
 
@@ -70,16 +70,16 @@ f_water_status_sphagnum <- function(.) {
 # Calculates Sphagnum fresh weight : dry weight ratio as a function of water level (mm) - linear
 f_fwdw_wtd_lin <- function(.) {
   
-  if((.$env$water_td - .$env$sphag_h) > 0) .$pars$fwdw_wl_sat 
-  else .$pars$fwdw_wl_sat + .$pars$fwdw_wl_slope * -(.$env$water_td - .$env$sphag_h) 
+  if((.super$env$water_td - .super$env$sphag_h) > 0) .super$pars$fwdw_wl_sat 
+  else .super$pars$fwdw_wl_sat + .super$pars$fwdw_wl_slope * -(.super$env$water_td - .super$env$sphag_h) 
 }
 
 # Calculates Sphagnum fresh weight : dry weight ratio as a function of water level (mm) - exponential
 f_fwdw_wtd_exp <- function(.) {
   # Strack & Price 2009
   
-  if((.$env$water_td - .$env$sphag_h) > 0) exp( .$pars$fwdw_wl_exp_b + .$pars$fwdw_wl_exp_a*0 )
-  else exp( .$pars$fwdw_wl_exp_b + .$pars$fwdw_wl_exp_a * (-(.$env$water_td - .$env$sphag_h)/10) ) 
+  if((.super$env$water_td - .super$env$sphag_h) > 0) exp( .super$pars$fwdw_wl_exp_b + .super$pars$fwdw_wl_exp_a*0 )
+  else exp( .super$pars$fwdw_wl_exp_b + .super$pars$fwdw_wl_exp_a * (-(.super$env$water_td - .super$env$sphag_h)/10) ) 
 }
 
 
--- src/system_models/canopy_structure/canopy_structure_system_functions.R
+++ src/system_models/canopy_structure/canopy_structure_system_functions.R
@@ -10,59 +10,54 @@
 
 # Leaf demography  
 ###############################
-f_canstrctsys_leafdem <- function(.) {
+f_sys_leafdem <- function(.) {
 
   # set correct canopy output
-  .$canopy$cpars$output <- 'leaf_dem'    
+  .super$canopy$cpars$output[] <- 'leaf_dem'    
   
   # initialise canopy_structure
-  .$state$lai <- get(.$fnames$lai)(.)
+  .super$state$lai[] <- .$lai()
   
   # pass LAI to canopy object
-  .$canopy$fnames$lai <- 'f_lai_constant' 
-  .$canopy$pars$lai   <- .$state$lai 
+  .super$canopy$fnames$lai[] <- 'f_lai_constant' 
+  .super$canopy$pars$lai[]   <- .super$state$lai 
 
   # calculate fractions of leaf cohorts in upper and lower canopy
-  .$state$upper_can_prop <- get(.$fnames$leafdem_upper)(.)
-  .$state$lower_can_prop <- get(.$fnames$leafdem_lower)(.)
+  .super$state$upper_can_prop <- .$leafdem_upper()
+  .super$state$lower_can_prop <- .$leafdem_lower()
 
   # fix vcmax as a constant at the leaf level 
-  .$canopy$leaf$fnames$vcmax  <- 'f_vcmax_constant'  
+  .super$canopy$leaf$fnames$vcmax  <- 'f_vcmax_constant'  
 
   # scale vcmax as decreasing exponential function through the canopy 
-  .$canopy$fnames$vcmax0      <- 'f_vcmax0_constant'  
-  .$canopy$fnames$scale_vcmax <- 'f_scale_vcmax_beerslaw'  
+  .super$canopy$fnames$vcmax0      <- 'f_vcmax0_constant'  
+  .super$canopy$fnames$scale_vcmax <- 'f_scale_vcmax_beerslaw'  
 
   # calculate vcmax for leaf cohorts
-  .$state$leafdem_traits$canopy.vcmax0 <- get(.$fnames$leafdem_vcmax0)(.)
+  .super$state$leafdem_traits$canopy.vcmax0 <- .$leafdem_vcmax0()
 
   # initialise layers
-  #linc           <- .$state$lai / .$pars$layers
-  #ca_calc_points <- seq(linc, .$state$lai, linc ) 
-  #layers         <- .$pars$layers # this could be a function to dynamically specify the no. of layers 
-  .$canopy$pars$layers <- .$pars$layers     # this could be a function to dynamically specify the no. of layers 
-  .$init_vert(l=.$pars$layers)              # reallocating this memory is unnecessary in cases where layers is a fixed parameter. 
+  .super$canopy$pars$layers <- .super$pars$layers     # this could be a function to dynamically specify the no. of layers 
+  .super$init_vert(.=.super, l=.super$pars$layers)    # reallocating this memory is unnecessary in cases where layers is a fixed parameter. 
  
   # run canopy for each leaf cohort
   # create canopy parameter matrix
   lcohorts <- 3
-  cohortmatrix <- vapply(.$state$leafdem_traits[c('canopy.vcmax0','canopy.vcmax0')], function(v) v, numeric(lcohorts) )
+  cohortmatrix <- vapply(.super$state$leafdem_traits[c('canopy.vcmax0','canopy.vcmax0')], function(v) v, numeric(lcohorts) )
   #print(cohortmatrix)
   #print(class(cohortmatrix))
   #print(colnames(cohortmatrix))
 
   # run canopy - need to output layer matrix 
-  #canopy_out <- vapply(1:lcohorts, .$run_canopy, matrix(0,.$pars$layers,length(.$canopy$output()), df=cohortmatrix )
-  canopy_out <- vapply(1:lcohorts, .$run_canopy, matrix(0,.$pars$layers,length(.$canopy$state$vert$layer)), df=cohortmatrix )
-  #canopy_out <- vapply(1:lcohorts, .$run_canopy, .$canopy$output(), df=cohortmatrix )
+  canopy_out <- vapply(1:lcohorts, .$run_canopy, matrix(0,.super$pars$layers,length(.super$canopy$state$vert$layer)), df=cohortmatrix )
 
   # assign data to canopy_structure object data structure
   for(vname in colnames(canopy_out)) {
-    .$state$vert$young[[vname]][]  <- canopy_out[,vname,1]
-    .$state$vert$mature[[vname]][] <- canopy_out[,vname,2]
-    .$state$vert$old[[vname]][]    <- canopy_out[,vname,3]
+    .super$state$vert$young[[vname]][]  <- canopy_out[,vname,1]
+    .super$state$vert$mature[[vname]][] <- canopy_out[,vname,2]
+    .super$state$vert$old[[vname]][]    <- canopy_out[,vname,3]
   }
-  if(.$cpars$verbose) {
+  if(.super$cpars$verbose) {
     print('Leaf cohorts:', quote=F )
     print(cohortmatrix)
     print(canopy_out)
@@ -70,30 +65,30 @@ f_canstrctsys_leafdem <- function(.) {
 
 
   # combine leaf cohorts 
-  linc  <- .$state$lai / .$pars$layers
-  upper <- floor(.$pars$lai_upper / linc) 
-  for(vname in names(.$state$vert$layer)) 
-    .$state$vert$layer[[vname]][1:upper] <- 
-      .$state$vert$young[[vname]][1:upper]  * .$state$upper_can_prop[1] + 
-      .$state$vert$mature[[vname]][1:upper] * .$state$upper_can_prop[2] +
-      .$state$vert$old[[vname]][1:upper]    * .$state$upper_can_prop[3] 
+  linc  <- .super$state$lai / .super$pars$layers
+  upper <- floor(.super$pars$lai_upper / linc) 
+  for(vname in names(.super$state$vert$layer)) 
+    .super$state$vert$layer[[vname]][1:upper] <- 
+      .super$state$vert$young[[vname]][1:upper]  * .super$state$upper_can_prop[1] + 
+      .super$state$vert$mature[[vname]][1:upper] * .super$state$upper_can_prop[2] +
+      .super$state$vert$old[[vname]][1:upper]    * .super$state$upper_can_prop[3] 
 
   # is this the correct way to scale rs etc? --- no, best to convert to gs
-  for(vname in names(.$state$vert$layer)) 
-    .$state$vert$layer[[vname]][(upper+1):.$pars$layers] <- 
-       .$state$vert$young[[vname]][(upper+1):.$pars$layers]  * .$state$lower_can_prop[1] + 
-       .$state$vert$mature[[vname]][(upper+1):.$pars$layers] * .$state$lower_can_prop[2] +
-       .$state$vert$old[[vname]][(upper+1):.$pars$layers]    * .$state$lower_can_prop[3] 
+  for(vname in names(.super$state$vert$layer)) 
+    .super$state$vert$layer[[vname]][(upper+1):.super$pars$layers] <- 
+       .super$state$vert$young[[vname]][(upper+1):.super$pars$layers]  * .super$state$lower_can_prop[1] + 
+       .super$state$vert$mature[[vname]][(upper+1):.super$pars$layers] * .super$state$lower_can_prop[2] +
+       .super$state$vert$old[[vname]][(upper+1):.super$pars$layers]    * .super$state$lower_can_prop[3] 
 
   # integrate canopy_structure layers
   # canopy_structure sum values
   for(vname in c('apar','A','gb','gs','gi','g','rd','Acg_lim','Ajg_lim','Apg_lim') ) {
-    .$state$integrated[[vname]][] <- sum(.$state$vert$layer[[vname]]) * linc
+    .super$state$integrated[[vname]][] <- sum(.super$state$vert$layer[[vname]]) * linc
   }
   # canopy_structure mean values 
-  .$state$integrated$cb[] <- sum(.$state$vert$layer$cb) / .$state$lai * linc
-  .$state$integrated$cc[] <- sum(.$state$vert$layer$cc) / .$state$lai * linc
-  .$state$integrated$ci[] <- sum(.$state$vert$ci)       / .$state$lai * linc
+  .super$state$integrated$cb[] <- sum(.super$state$vert$layer$cb) / .super$state$lai * linc
+  .super$state$integrated$cc[] <- sum(.super$state$vert$layer$cc) / .super$state$lai * linc
+  .super$state$integrated$ci[] <- sum(.super$state$vert$ci)       / .super$state$lai * linc
   
 }
 
