462ca60c545928adf04ca719fb6ec2f2145ea484
walkeranthonyp <walkeranthonyp@gmail.com>
2022-05-02 18:57:37 -0400

    bugfix soil_decomp: add tryCatch error handling to solvers

--- src/system_models/soil_decomp/soil_decomp_system_functions.R
+++ src/system_models/soil_decomp/soil_decomp_system_functions.R
@@ -11,17 +11,35 @@
 ################################
 # soil_decomp system function 
 
+# a system of n pools, composed of a single dynamic solver function
 f_sys_npools <- function(.) {
-  .super$state_pars$solver_out <- .$solver(.super$state$cpools, c(0,1), .$solver_func )
-  .super$state$cpools[,1]      <- .super$state_pars$solver_out[dim(.super$state_pars$solver_out)[1],2:(.super$pars$n_pools+1)]
+
+  .super$state_pars$solver_out <- tryCatch(
+    .$solver(.super$state$cpools, c(0,1), .$solver_func ), 
+    error = function(c) {
+      print(c)
+      matrix(ncol=.super$pars$n_pools+1)
+    })
+  .super$state$cpools[,1] <- .super$state_pars$solver_out[dim(.super$state_pars$solver_out)[1],2:(.super$pars$n_pools+1)]
 }
 
+
+# steady-state solver for a system of n pools
 f_steadystate_npools <- function(.) {
-  .super$state_pars$solver_steadystate_out <- .$solver_steadystate(.super$state$cpools, 0, func=.$solver_func )
-  .super$state$cpools[,1]                  <- .super$state_pars$solver_steadystate_out$y
+
+  .super$state_pars$solver_steadystate_out <- tryCatch(
+    .$solver_steadystate(.super$state$cpools, 0, func=.$solver_func )
+    error = function(c) {
+      print(c)
+      list(y=rep(NA,.super$pars$n_pools))
+    })
+  .super$state$cpools[,1] <- .super$state_pars$solver_steadystate_out$y
 }
 
+
+# NULL solver to prevent steady-state initialisation, i.e. to initialise with values specified in input
 f_steadystate_null   <- function(.) NULL 
+
 
 
 ### END ###
