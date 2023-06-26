b4b04a3cfe1db7f5aae963b069d28c322ef87a9d
Siedahmed <fs8@mbp116872.ornl.gov>
2023-06-26 11:43:40 -0400

    Deleted unnecessary files

--- interface/MAAT/install_dependencies.R
+++
@@ -1,9 +0,0 @@
-# install packages needed from CRAN
-list.of.packages <- c("proto","XML","xtable","randtoolbox","deSolve")
-new.packages     <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-
-if (length(new.packages)) {
-  print("installing : ")
-  print(new.packages)
-  install.packages(new.packages, repos = "http://cran.rstudio.com/", dependencies = TRUE)
-}
